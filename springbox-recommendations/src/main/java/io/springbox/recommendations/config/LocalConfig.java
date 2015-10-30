ef7513d2756c50d7867ba53c61f7eb9bec9a394a
Adrian Ursu <Adrian.Ursu@opencredo.com>
2015-10-30 12:31:15 +0000

    Stuff to make it work

--- springbox-recommendations/src/main/java/io/springbox/recommendations/config/LocalConfig.java
+++ springbox-recommendations/src/main/java/io/springbox/recommendations/config/LocalConfig.java
@@ -1,6 +1,7 @@
 package io.springbox.recommendations.config;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,9 +11,15 @@ import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 @Profile("default")
 public class LocalConfig {
 
+    @Value("${spring.data.neo4j.host}")
+    private String neo4jHost;
+
+    @Value("${spring.data.neo4j.port}")
+    private String neo4jPort;
+
     @Bean
     public GraphDatabaseService graphDatabaseService() {
-        return new SpringRestGraphDatabase("http://localhost:7474/db/data/");
+        return new SpringRestGraphDatabase("http://"+neo4jHost+":"+neo4jPort+"/db/data/");
     }
 
 }
